fd92d76879015f1ef4fd037aee7a9021f62a5bba
Butkeim <yanis.kerrad@epitech.eu>
2020-04-09 17:36:06 +0200

    systeme api

--- app/src/main/java/com/example/trajet/ApiTrajet.java
+++ app/src/main/java/com/example/trajet/ApiTrajet.java
@@ -1,12 +1,9 @@
 package com.example.trajet;
 
-import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
-import android.view.View;
 
 import org.jetbrains.annotations.NotNull;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
@@ -18,19 +15,16 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-import static androidx.core.content.ContextCompat.startActivity;
-
 public class ApiTrajet {
 
     String reponsehttp = null;
-
 
     public String Trajet(String origin_addresses, String destination_addresses) throws InterruptedException {
     Log.i("reponse",""+origin_addresses+destination_addresses);
 
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("https://maps.googleapis.com/maps/api/distancematrix/json?origins="+origin_addresses+"&destinations="+destination_addresses+"&key=AIzaSyArK9hpQKzMOgf3a_LlWW6z7mIDxKoRE7o")
+                .url("https://maps.googleapis.com/maps/api/distancematrix/json?origins="+origin_addresses+"&destinations="+destination_addresses+"&key="+BuildConfig.apiKey)
                 .build();
 
 
